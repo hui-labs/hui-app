21e216254088688a6cb212bbe0774118d244882b
Tran Tuan Linh <linh.tran@linh-tran-SM-MBP-E.local>
2022-11-19 18:27:56 +0700

    feat: update contract

---
+++ app/assetbacked-web/pages/index.tsx
@@ -0,0 +1,10 @@
+import Connect2Phantom from "../component/Connect2Phantom"
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Connect2Phantom />
+    </div>
+  )
+}
