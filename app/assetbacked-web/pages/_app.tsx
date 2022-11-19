8312c17faf8a5047fcbd792021e9a80997ddee48
linrium <linrium@gmail.com>
2022-11-19 20:05:19 +0700

    update

--- app/assetbacked-web/pages/_app.tsx
+++ app/assetbacked-web/pages/_app.tsx
@@ -1,5 +1,24 @@
 import type { AppProps } from "next/app"
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom"
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react"
+import { endpoint } from "./api/constants"
+import "@solana/wallet-adapter-react-ui/styles.css"
+import "../styles/globals.css"
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const phantomWallet = new PhantomWalletAdapter()
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[phantomWallet]}>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
 }
--- app/assetbacked-web/pages/index.tsx
+++ app/assetbacked-web/pages/index.tsx
@@ -1,10 +1,20 @@
-import Connect2Phantom from "../component/Connect2Phantom"
+import useIsMounted from "../hooks/useIsMounted"
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
+import styles from "../styles/Home.module.css"
 
 export default function Home() {
+  const mounted = useIsMounted()
+
   return (
-    <div>
-      <h1>Hello</h1>
-      <Connect2Phantom />
+    <div className={styles.container}>
+      <div className={styles.navbar}>{mounted && <WalletMultiButton />}</div>
+
+      <div className={styles.main}>
+        <h1 className={styles.title}>
+          Your First Solana Program with{" "}
+          <a href="https://alchemy.com/solana/?a=d0c917f7ef">Alchemy</a>!
+        </h1>
+      </div>
     </div>
   )
 }
