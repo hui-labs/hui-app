7a1bcde5f4bbc48624876d316855a6fbf741754e
phieu <phieu.yiu@gmail.com>
2022-11-19 16:33:46 +0700

    feat: init assetbacked-web

--- app/assetbacked-mobile/App.tsx
+++ app/assetbacked-mobile/App.tsx
@@ -1,11 +1,7 @@
-import { StatusBar } from "expo-status-bar"
-import { Text, View } from "react-native"
+import { SafeAreaView } from "react-native"
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white"></SafeAreaView>
   )
 }
---
+++ app/assetbacked-web/pages/index.tsx
@@ -0,0 +1,10 @@
+import ConnectToPhantom from "../component/ConnectToPhantom"
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <ConnectToPhantom />
+    </div>
+  )
+}
